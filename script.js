61cb7e1d19010fa0c42d1b667edb48c36aa856c5
jancarloa0524 <jancarloa0524@hstat.org>
2022-01-09 02:50:13 -0500

    updated with buyingACar.html code

--- script.js
+++ script.js
@@ -82,3 +82,52 @@ function isIsogram(str){
 // true
 // console.log(isIsogram("LET'S GOOOOOOOOOOOO"))
 // false
+
+
+
+
+
+  // INPUT: Starting price of old car; Starting price of new car; Savings per month; Percent loss per month of both cars
+  // ALG: 
+      // 1: First, I need to establish the values of some variables used to assist in calculations
+      // 2: Run a "for" loop which performs calculations for each month.
+          // 1. Check if the old car costs more then/equal to new car.
+          // 2. Check if month is divisible by 2, and not equal to zero (to exlude 0th month)
+          // 3. If month is 0, set final answer to (CALCULATION 1). If not, set final answer to (CALCULATION 2)
+      // 3: Return the final answer, which is the OUTPUT
+  // OUTPUT: Number of months, difference between savingsPerMonth and startPriceNew, ONLY when the car is affordable
+  function nbMonths(startPriceOld, startPriceNew, savingperMonth, percentLossByMonth){
+    var savingsPlusOld = savingperMonth; // Savings + Old Price (total savings) is created, due to the fact that the car will be sold to assist payment of new car. Set to savingsPerMonth, so that the "for" loop works as intended
+    var finalAnswer; // This will be returned later on
+    percentLossByMonth = percentLossByMonth / 100 // Finds decimal value of percentage
+    for (var month = 0; savingsPlusOld < startPriceNew; month++) { // Repeats until total savings > the price of the New Car, now affordable
+        if (startPriceOld => startPriceNew) { // If the price of the old car > new car, there is no need to save and thus the payment is made in first month
+            savingsPlusOld = startPriceOld; // Sets total savings to Old Price, so that the loop does not repeat
+        }
+        if (month % 2 == 0 && month != 0) { // Every two months, +0.5%
+            percentLossByMonth = percentLossByMonth + 0.005; 
+        }
+        // The following is a ternary that calculates differently if the month is or isn't 0.
+        // IF 0, simply Old Price - New Price. No savings or percentages are applied until the end of the first month. These are the initial values and matter when the Old Price > New Price. 
+        // IF NOT 0, apply percentages to Old and New Price, find total savings, and total savings - New Price to find difference. 
+            // Note: These cannot be put into one equation due Old/New car changing prices. So every month must be less then the last. Total saivngs also increases every month. 
+        month == 0 ? (finalAnswer = [month, Math.round(startPriceOld - startPriceNew)]) : (startPriceOld = startPriceOld - (startPriceOld * percentLossByMonth), startPriceNew = startPriceNew - (startPriceNew * percentLossByMonth), savingsPlusOld = (savingperMonth * month) + startPriceOld, finalAnswer = [month, Math.round(savingsPlusOld - startPriceNew)])
+
+        // The following is an easier to undersatand if/else statement. It is the exact same code as the ternary.
+        /* if (month == 0) {
+            finalAnswer = [month, Math.round(startPriceOld - startPriceNew)];
+        } else {
+        startPriceOld = startPriceOld - (startPriceOld * percentLossByMonth);
+        startPriceNew = startPriceNew - (startPriceNew * percentLossByMonth);
+        savingsPlusOld = (savingperMonth * month) + startPriceOld;
+        finalAnswer = [month, Math.round(savingsPlusOld - startPriceNew)];
+        }
+        */
+    }
+    return finalAnswer;
+}
+// Testing
+console.log(nbMonths(2000, 8000, 1000, 1.5)); // should return [6, 766]
+console.log(nbMonths(12000, 8000, 1000, 1.5)); // should return [0, 4000]
+console.log(nbMonths(8000, 8000, 1000, 1.5)); // should return [0, 0]
+console.log(nbMonths(2500, 3600, 1000, 1.2)); // should return [2, 932]
